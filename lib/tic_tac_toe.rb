d0ef0e53f866f7db7e7470f238bf8e813d52856e
Dawn Catanach <dawn.catanach@gmail.com>
2018-04-04 00:02:52 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,17 @@
+#Helper methods 
+
+WIN_COMBINATIONS = [
+  [0,1,2], #top row win 
+  [3,4,5], #middle row win 
+  [6,7,8], #bottom row win 
+  [0,3,6], #left column win 
+  [1,4,7], #center column win 
+  [2,5,8], #right column win 
+  [0,4,8], #left diagonal win 
+  [2,4,6] #right diagonal win 
+]
+
+def display_board(board) 
+  #code goes here, once I find my finished work 
+end 
 
