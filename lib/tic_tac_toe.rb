a0dd34a4927fe2ff26e516922ccc988055f3d23d
Dawn Catanach <dawn.catanach@gmail.com>
2018-04-04 01:00:41 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,7 +20,7 @@ def display_board(board)
 end
 
 def input_to_index(user_input)
-  user_input.to_i - 1 
+  user_input.to_i - 1
 end
 
 def move(board, index, current_player = "X")
@@ -31,8 +31,8 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-def valid_move?()
-  #code
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def turn()
