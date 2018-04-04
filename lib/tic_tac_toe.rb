596e129044b2f29e68ad1cb27b78515146c2e498
Dawn Catanach <dawn.catanach@gmail.com>
2018-04-04 00:08:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,3 +42,9 @@ end
 def current_player()
   #code
 end
+
+def won?(board) 
+  WIN_COMBINATIONS.detect do |combination| 
+    board[combination[0]] == board[combination[1]] && board[combination[1]] == board[combination[2]] && position_taken?(board, combination[0]) 
+  end #close block 
+end #close method definition 
