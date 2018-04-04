5ce37b33020f9d630f3e2bdcf6911ce16360aab0
Dawn Catanach <dawn.catanach@gmail.com>
2018-04-04 01:10:31 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,7 +24,7 @@ def input_to_index(user_input)
 end
 
 def move(board, index, current_player_token)
-  board[index] = current_player
+  board[index] = current_player_token 
 end
 
 def position_taken?(board, index)
